594b4e5fab174fa874df8643e80957c1bcd227f8
Mintdot <nigthmare100@naver.com>
2017-08-09 14:14:16 +0900

    firebase connect

---
+++ app/src/main/java/com/smartspatial/sschat/ChatActivity.java
@@ -0,0 +1,8 @@
+package com.smartspatial.sschat;
+
+/**
+ * Created by nigth on 2017-08-08.
+ */
+
+public class ChatActivity {
+}
--- app/src/main/java/com/smartspatial/sschat/MainActivity.java
+++ app/src/main/java/com/smartspatial/sschat/MainActivity.java
@@ -3,11 +3,14 @@ package com.smartspatial.sschat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.facebook.login.widget.LoginButton;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        LoginButton loginButton = (LoginButton) findViewById(R.id.button_facebook_login);
     }
 }
