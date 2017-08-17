ee7435a9b4ddd479b86dd5835b1a410fbb8312a8
Mintdot <nigthmare100@naver.com>
2017-08-17 16:57:20 +0900

    Add Toolbar and ImageButton

--- app/src/main/java/com/smartspatial/sschat/MainActivity.java
+++ app/src/main/java/com/smartspatial/sschat/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
@@ -33,6 +34,9 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         findViewById(R.id.logout_button).setOnClickListener(new View.OnClickListener() {
             @Override
